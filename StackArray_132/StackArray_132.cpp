7969429e05762dfe5e53d8204ffd2fbbc5489c83
dewikhulbi <dewikhlb20@gmail.com>
2023-06-15 15:36:49 +0700

    membuat int main

--- StackArray_132/StackArray_132.cpp
+++ StackArray_132/StackArray_132.cpp
@@ -61,7 +61,16 @@ int main() {
 		cout << "3. display\n";
 		cout << "4. exit\n";
 		cout << "enter your choice : ";
-		string inout;
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+		switch (ch) {
+		case '1': {
+			cout << "\nenter a element : ";
+			string element;
+			getline(cin, element);
+		}
+		}
 
 	}
 }
